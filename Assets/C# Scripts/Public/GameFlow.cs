4fc707e03e1b308e7c373f5f4f5bbdca4b94dba9
DESKTOP-3MO854A\shi_xian <azxczxc1125@gmail.com>
2024-08-03 16:42:27 +0800

    feat:角色爬梯動畫，串接爬梯按鈕，封裝PlayerConTrol移動的程式碼

--- Assets/C# Scripts/Public/GameFlow.cs
+++ Assets/C# Scripts/Public/GameFlow.cs
@@ -6,23 +6,15 @@ using UnityEngine;
 public class GameFloW : MonoBehaviour
 {
     [SerializeField] private KeyboardSettings keyboardSettings;
-    [SerializeField] private PlayerControl playerControl;
+    [SerializeField] private GameObject Player;
+    [SerializeField] private IPlayer playerControl;
     private void Awake()
     {
+        playerControl = Player.GetComponent<PlayerControl>();
         keyboardSettings.Move += playerControl.Move;
         keyboardSettings.Jump += playerControl.Jump;
-        keyboardSettings.Crouch += playerControl.Crouch;
-        keyboardSettings.Attack+= playerControl.Attack;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        keyboardSettings.Crouch += playerControl.CrouchAndStairsDown;
+        keyboardSettings.Attack += playerControl.Attack;
+        keyboardSettings.StairsUp+=playerControl.StairsUp;
     }
 }
--- Assets/C# Scripts/Public/IPlayer.cs
+++ Assets/C# Scripts/Public/IPlayer.cs
@@ -1,7 +1,10 @@
 
 public interface IPlayer
 {
+    public void Move(float movePressed);
     public void Jump(bool jumpPressed);
-    public void Attack();
-    public void Crouch();
+    public void StairsUp(bool StairsUpPressed);
+    public void CrouchAndStairsDown(bool crouchPressed);
+    public void Attack(bool attackPressed);
+    
 }
