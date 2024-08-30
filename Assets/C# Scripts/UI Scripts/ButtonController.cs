b3573e26674465123adbd07bef08b3c97cba4c0d
DESKTOP-3MO854A\shi_xian <azxczxc1125@gmail.com>
2024-08-30 17:15:34 +0800

    feat:技能程式優化

--- Assets/C# Scripts/UI Scripts/ButtonController.cs
+++ Assets/C# Scripts/UI Scripts/ButtonController.cs
@@ -13,10 +13,6 @@ public class ButtonController : MonoBehaviour
 
     [SerializeField] GameObject game;
 
-    [SerializeField] bool CheckSkill;
-    [SerializeField] int HPSkillNumber;
-    [SerializeField] int DmageSkillNumber;
-    [SerializeField] int jumpSkillNumber;
 
     public event Func<int> CherryValue;
     public event Action<int> DeductCherryValue;
@@ -24,11 +20,22 @@ public class ButtonController : MonoBehaviour
     public event Action SetJumpValue;
     public event Action<float> SetHPValue;
 
+    [SerializeField] private Skill skill;
+    private enum Skill
+    {
+        HPSkill_level_1,
+        HPSkill_level_2,
+        AttackSkill_level_1,
+        AttackSkill_level_2,
+        jumpSkill,
+        NoPoint
+    }
 
-    bool level_one_done;
+    private Dictionary<Skill, bool> skillUnlockStatus = new Dictionary<Skill, bool>();
     private void Awake()
     {
-        
+        skillUnlockStatus.Add(Skill.HPSkill_level_2, false);
+        skillUnlockStatus.Add(Skill.AttackSkill_level_2, false);
     }
     public void OnBackClicked()
     {
@@ -38,73 +45,65 @@ public class ButtonController : MonoBehaviour
     // Skill Set 1
     public void OnSkillset_1Clicked()
     {
-        RestSkill();
         textbox01.text = "As you enhance your vitality, your health pool experiences a significant boost. Leveling up your HP grants you increased resilience, allowing you to withstand more damage in the face of adversity. Hp + 1";
         game = GameObject.Find("Skill_Set _1");
-        
-        if (CherryValue.Invoke()  >= 3)
-        {
-            CheckSkill = true;
-            HPSkillNumber = 1;
-            textbox02.text = "Upgradeable : YES";
-        }
-        else 
-        {
-            CheckSkill = false;
-            textbox02.text = "Upgradeable : NO <br>Need 3 Cherry to Upgrade";
-        }   
-    }
 
-    public void OnSkillset_1_1Clicked()
-    {
-        RestSkill();
-        textbox01.text = "With this advanced upgrade, not only does your health increase further, but your endurance reaches a new level of mastery. You'll find yourself enduring through challenging battles with greater stamina, ensuring prolonged survival in the toughest encounters. Hp + 1";
-        game = GameObject.Find("Skill_Set _1.1");
-        if (CherryValue.Invoke() >= 3 && level_one_done == true)
+        if (CherryValue.Invoke() >= 3)
         {
-            CheckSkill = true;
-            HPSkillNumber = 2;
+            skill = Skill.HPSkill_level_1;
             textbox02.text = "Upgradeable : YES";
         }
         else
         {
-            CheckSkill = false;
+            skill = Skill.NoPoint;
+            textbox02.text = "Upgradeable : NO <br>Need 3 Cherry to Upgrade";
+        }
+    }
+
+    public void OnSkillset_1_1Clicked()
+    {
+        textbox01.text = "With this advanced upgrade, not only does your health increase further, but your endurance reaches a new level of mastery. You'll find yourself enduring through challenging battles with greater stamina, ensuring prolonged survival in the toughest encounters. Hp + 1";
+        game = GameObject.Find("Skill_Set _1.1");
+        if (CherryValue.Invoke() >= 3 && skillUnlockStatus[Skill.HPSkill_level_2] == true)
+        {
+            skill = Skill.HPSkill_level_2;
+            textbox02.text = "Upgradeable : YES";
+        }
+        else
+        {
+            skill = Skill.NoPoint;
             textbox02.text = "Upgradeable : NO <br>Need 3 Cherry to Upgrade & Hp level 1 upgraded";
         }
     }
 
     public void OnSkillset_2Clicked()
     {
-        RestSkill();
         textbox01.text = "Sharpen your combat skills to increase the power of your attacks. The level 1 upgrade in attack enhances your combat proficiency, dealing more damage to adversaries. Feel the surge of strength as you unleash devastating blows on your foes. Attack Damage + 2";
         game = GameObject.Find("Skill_Set _2");
         if (CherryValue.Invoke() >= 3)
         {
-            DmageSkillNumber = 1;
-            CheckSkill = true;
+            skill = Skill.AttackSkill_level_1;
             textbox02.text = "Upgradeable : YES";
         }
         else
         {
-            CheckSkill = false;
+            skill = Skill.NoPoint;
             textbox02.text = "Upgradeable : NO <br>Need 3 Cherry to Upgrade";
-        }     
+        }
     }
-    
+
     public void OnSkillset_2_1Clicked()
     {
-        RestSkill();
         textbox01.text = "Ascend to the next level of mastery with your weapon of choice. The level 2 upgrade not only boosts your attack power but also unlocks mastery strikes. Execute intricate and powerful combos, overwhelming enemies with a dazzling display of skill and precision. Attack Damage + 4";
         game = GameObject.Find("Skill_Set _2.1");
-        if (CherryValue.Invoke() >= 3 && level_one_done == true)
+        if (CherryValue.Invoke() >= 3 && skillUnlockStatus[Skill.AttackSkill_level_2] == true)
         {
-            DmageSkillNumber = 2;
-            CheckSkill = true;
+            skill = Skill.AttackSkill_level_2;
             textbox02.text = "Upgradeable : YES";
         }
         else
         {
-            CheckSkill = false;
+            skill = Skill.NoPoint;
             textbox02.text = "Upgradeable : NO <br>Need 3 Cherry to Upgrade & Attack level 1 upgraded";
         }
     }
@@ -112,105 +111,73 @@ public class ButtonController : MonoBehaviour
 
     public void OnSkillset_3Clicked()
     {
-        RestSkill();
         textbox01.text = "Unlock the ability to perform a double jump, allowing you to reach higher platforms and evade obstacles with finesse. Master the art of the airborne leap to explore new areas and gain a tactical advantage in traversing the game world.";
         game = GameObject.Find("Skill_Set _3");
         if (CherryValue.Invoke() >= 3)
         {
-            jumpSkillNumber = 1;
-            CheckSkill = true;
+            skill = Skill.jumpSkill;
             textbox02.text = "Upgradeable : YES";
         }
         else
         {
-            CheckSkill = false;
+            skill = Skill.NoPoint;
             textbox02.text = "Upgradeable : NO <br>Need 3 Cherry to Upgrade";
         }
-      
-    }
-
-
-    public void OnCheckerButton() 
-    {
-        if (HPSkillNumber == 1)
-        {
-            if (CheckSkill == true)
-            {
-                DeductCherryValue.Invoke(3);
-                SetHPValue.Invoke(5f);
-
-                game.GetComponent<Button>().interactable = false;
-                level_one_done = true;
-
-                textbox02.text = " ";
-            }
-            else { return; }
-
-
-        }
-        else if (HPSkillNumber == 2)
-        {
-            if (CheckSkill == true) 
-            {
-                DeductCherryValue.Invoke(3);
-                SetHPValue.Invoke(7f);
-
-                game.GetComponent<Button>().interactable = false;
-                textbox02.text = " ";
-            }
-            else { return; }
-
-        }
-       
-        else if (DmageSkillNumber == 1)
-        {
-            if (CheckSkill == true)
-            {
-                DeductCherryValue.Invoke(3);
-                SetDamage.Invoke(3);
-
-                game.GetComponent<Button>().interactable = false;
-                level_one_done = true;
-
-                textbox02.text = " ";
-            }
-            else { return; }
-
-
-        }
-        else if (DmageSkillNumber == 2)
-        {
-            if (CheckSkill == true)
-            {
-                DeductCherryValue.Invoke(3);
-                SetDamage.Invoke(7);
-
-                game.GetComponent<Button>().interactable = false;
-                textbox02.text = " ";
-            }
-            else { return; }
-
-
-        }
-      
-        else if (jumpSkillNumber == 1)
-        {
-            if (CheckSkill == true)
-            {
-                DeductCherryValue.Invoke(3);
-                SetJumpValue.Invoke();
-
-                game.GetComponent<Button>().interactable = false;
-                textbox02.text = " ";
-            }
-            else { return; }
-        }
 
     }
-    void RestSkill() 
+
+
+    public void OnCheckerButton()
     {
-        HPSkillNumber = 0;
-        DmageSkillNumber = 0;
-        jumpSkillNumber = 0;
+        switch (skill)
+        {
+            case Skill.NoPoint:
+                break;
+
+            case Skill.HPSkill_level_1:
+                skillUnlockStatus[Skill.HPSkill_level_2] = true;
+                UpgradeSkillHP(5f);
+
+                break;
+
+            case Skill.HPSkill_level_2:
+                UpgradeSkillHP(7f);
+                break;
+
+            case Skill.AttackSkill_level_1:
+                skillUnlockStatus[Skill.AttackSkill_level_2] = true;
+                UpgradeSkillAttack(3);
+                break;
+            case Skill.AttackSkill_level_2:
+                UpgradeSkillAttack(7);
+                break;
+            case Skill.jumpSkill:
+                UpgradeSkillJump();
+                break;
+        }
+    }
+    private void UpgradeSkillHP(float number)
+    {
+        DeductCherryValue.Invoke(3);
+        SetHPValue.Invoke(number);
+        game.GetComponent<Button>().interactable = false;
+        textbox02.text = " ";
+        skill = Skill.NoPoint;
+    }
+    private void UpgradeSkillAttack(int number)
+    {
+        DeductCherryValue.Invoke(3);
+        SetDamage.Invoke(number);
+        game.GetComponent<Button>().interactable = false;
+        textbox02.text = " ";
+        skill = Skill.NoPoint;
+    }
+    private void UpgradeSkillJump()
+    {
+        DeductCherryValue.Invoke(3);
+        SetJumpValue.Invoke();
+        game.GetComponent<Button>().interactable = false;
+        textbox02.text = " ";
+        skill = Skill.NoPoint;
     }
 }
